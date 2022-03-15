a6c1fb867d58894efe8e750234756d6e25c9fe31
Marino Signorini <98362587+marinosig@users.noreply.github.com>
2022-03-15 15:56:01 +0000

    Add files via upload

---
+++ new.js
@@ -0,0 +1,10 @@
+// Declare and write your function here:
+function standardsMaker() {
+
+    for (var loop1 = 0; loop1 < 300; loop1++) {
+        console.log("I will write questions if i'm stuck")
+    } 
+    
+}
+    
+    standardsMaker();
